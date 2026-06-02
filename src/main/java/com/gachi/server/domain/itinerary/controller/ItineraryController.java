d5f4d9a0f976c461465cdac533c24ad13a9ba6c5
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-06-02 20:36:59 +0900

    feat: Swagger 및 CORS 설정 추가

--- src/main/java/com/gachi/server/domain/itinerary/controller/ItineraryController.java
+++ src/main/java/com/gachi/server/domain/itinerary/controller/ItineraryController.java
@@ -1,10 +1,12 @@
 package com.gachi.server.domain.itinerary.controller;
 
 import com.gachi.server.domain.itinerary.service.ItineraryService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Itinerary", description = "일정 API")
 @RestController
 @RequestMapping("/api/travels/{travelId}/itinerary")
 @RequiredArgsConstructor
